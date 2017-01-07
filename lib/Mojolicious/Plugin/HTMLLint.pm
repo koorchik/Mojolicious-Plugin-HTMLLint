8ae75d2d80dc3619a4760c862494c111e309c316
koorchik <koorchik@gmail.com>
2017-01-07 18:02:38 +0200

    Update to HTML::Lint 2.26

--- lib/Mojolicious/Plugin/HTMLLint.pm
+++ lib/Mojolicious/Plugin/HTMLLint.pm
@@ -3,12 +3,15 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 use HTML::Lint;
 
-our $VERSION = '0.04';
+use v5.10; # earliest occurance of feature
+no warnings 'experimental::smartmatch';
+
+our $VERSION = '0.05';
 
 sub register {
     my ( $self, $app, $conf ) = @_;
     $conf ||= {};
-    
+
     my $skip = delete $conf->{skip} // [];
 
     # On error callback
@@ -18,7 +21,7 @@ sub register {
     } else {
         $on_error = sub { $app->log->warn($_[1]) };
     }
-   
+
     $app->hook(
         'after_dispatch' => sub {
             my ( $c ) = @_;
@@ -26,20 +29,21 @@ sub register {
 
             # Only successful response
             return if $res->code !~ m/^2/;
- 
+
             ## Only html response
             return unless $res->headers->content_type;
             return if $res->headers->content_type !~ /html/;
-            
+
             my $lint = HTML::Lint->new(%$conf);
             $lint->parse($res->body);
-            
+            $lint->eof();
+
             foreach my $error ( $lint->errors ) {
                 my $err_msg = $error->as_string();
                 next if $err_msg ~~ $skip;
 
                 $on_error->( $c, "HTMLLint:" . $error->as_string );
-            }            
+            }
         } );
 }
 
@@ -54,32 +58,32 @@ Mojolicious::Plugin::HTMLLint - HTML::Lint support for Mojolicious
 
   # Mojolicious
   $self->plugin('HTMLLint');
-  
-  # In development mode only 
-  $self->plugin('HTMLLint') if $self->mode eq 'development'; 
-  
+
+  # In development mode only
+  $self->plugin('HTMLLint') if $self->mode eq 'development';
+
   # Mojolicious (skip errors that contain "placeholder" string)
-  $self->plugin( 'HTMLLint', { skip => [ qr/placeholder/ ] } ); 
+  $self->plugin( 'HTMLLint', { skip => [ qr/placeholder/ ] } );
 
   # Mojolicious::Lite
   plugin 'HTMLLint';
 
 =head1 DESCRIPTION
 
-L<Mojolicious::Plugin::HTMLLint> - allows you to validate HTML rendered by your application. The plugin uses HTML::Lint for validation. Errors will appear in Mojolicious log. 
+L<Mojolicious::Plugin::HTMLLint> - allows you to validate HTML rendered by your application. The plugin uses HTML::Lint for validation. Errors will appear in Mojolicious log.
 
 =head1 CONFIG
 
-Config will be passed to HTML::Lint->new();  
+Config will be passed to HTML::Lint->new();
 For supported options see L<HTML::Lint>
 
-=head2 C<skip> 
+=head2 C<skip>
 
   $app->plugin('HTMLLint', { skip => [ qr//, qr// ]} );
 
 This options says what message not to show.   This option plugin processes by its own(without passing to HTML::Lint).
 
-=head2 C<on_error> 
+=head2 C<on_error>
 
 You can pass custom error handling callback. For example
 
@@ -100,6 +104,6 @@ Please report any bugs or feature requests to Github L<https://github.com/koorch
 
 =head1 SEE ALSO
 
-L<Mojolicious>, L<HTML::Lint>, L<HTML::Tidy>  
+L<Mojolicious>, L<HTML::Lint>, L<HTML::Tidy>
 
 =cut
--- t/basic.t
+++ t/basic.t
@@ -7,7 +7,7 @@ use Test::Mojo;
 
 my $error_string = '';
 
-plugin 'HTMLLint' => { on_error => sub { $error_string = $_[1] } };
+plugin 'HTMLLint' => { on_error => sub { $error_string = $_[1]; } };
 
 get '/wrongattr' => sub {
     my $self = shift;
@@ -21,13 +21,17 @@ get '/never_closed_div' => sub {
 
 my $t = Test::Mojo->new;
 
-$error_string = '';
-$t->get_ok('/wrongattr')->status_is(200)->content_like(qr{<a WRONGATTTR></a>});
-ok( $error_string =~ /Unknown attribute "wrongatttr"/, 'should be error about unknow attr');
+subtest 'wrongattr' => sub {
+  $error_string = '';
+  $t->get_ok('/wrongattr')->status_is(200)->content_like(qr{<a WRONGATTTR></a>});
+  ok( $error_string =~ /Unknown attribute "wrongatttr"/, 'should be error about unknown attr');
+};
 
-$error_string = '';
-$t->get_ok('/never_closed_div')->status_is(200)->content_like(qr{<div>test<div>});
-ok( $error_string =~ /div.+is never closed/, 'should be error about never closed div') ;
+subtest 'never_closed_div' => sub {
+  $error_string = '';
+  $t->get_ok('/never_closed_div')->status_is(200)->content_like(qr{<div>test<div>});
+  ok( $error_string =~ /div.+is never closed/, 'should be error about never closed div') ;
+};
 
 done_testing;
 
